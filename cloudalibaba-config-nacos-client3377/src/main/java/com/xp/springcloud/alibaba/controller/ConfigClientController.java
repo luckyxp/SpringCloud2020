c7cea709c672a77a1af7eb3c21800bc2610ea89c
luckyxp <2271613696@qq.com>
2020-05-19 16:12:49 +0800

    sentinel

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/xp/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.xp.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Climb.Xu
+ * @date 2020/5/18 0:15
+ */
+@RestController
+@RefreshScope //支持nacos动态刷新
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
